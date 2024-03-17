903c3e713df6fad5eb4a45de708fc18f8fa9aadc
Sulaiman_Mohammad <sulaiman.ayman.mohammad@gmail.com>
2024-03-17 20:51:58 +0100

    read raduis of commuincation and set it in expansion.h and pass it to python

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# Initialize variables
+n=""
+a="20"
 # Get the value of n from the input argument
 for arg in "$@"
 do
     case $arg in
         n=*)
-        n="${arg#n=}"
+        n="${arg#*=}"
+        ;;
+        a=*)
+        a="${arg#*=}"
         ;;
         *)
         echo "Invalid argument: $arg"
@@ -19,6 +25,23 @@ if [ ! -d "build" ]
 then
     mkdir build
 fi
+
+
+# Update expansion.h with the new value of a
+if [ -f "include/expansion.h" ]; then
+    # Create a temporary file
+    tmp_file=$(mktemp)
+
+    # Replace the line containing '#define a' with the new value
+    awk -v a_value="$a" '/#define a/{print "#define a " a_value; next} 1' include/expansion.h > "$tmp_file"
+
+    # Replace the original file with the temporary file
+    mv "$tmp_file" include/expansion.h
+else
+    echo "File include/expansion.h not found."
+    exit 1
+fi
+
 
 # Change to build directory and run cmake ..
 cd build
@@ -35,4 +58,4 @@ fi
 cd ..
 
 # Run the python script
-python plot_data.py $n
+python plot_data.py "$n" "$a"
